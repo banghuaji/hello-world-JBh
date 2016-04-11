be6a1fbaf5130540da18c68dbe8dc924f6e97edf
banghuaji <banghuaji@163.com>
2016-04-11 16:50:34 +0800

    add 5 files

---
+++ MyTestCaseAnnotation_3.java
@@ -0,0 +1,14 @@
+package com.focus.autotest.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.TYPE,ElementType.METHOD })
+public @interface MyTestCaseAnnotation {
+	public String caseMemo() default "12388888";
+}
